0b38a1f46c1084f7d3b6327dc4d5e7eaef23f56d
Anand Yadav <anandyadav0916@gmail.com>
2026-01-09 12:07:59 +0530

    Developer

--- app/layout.js
+++ app/layout.js
@@ -18,37 +18,40 @@ export default function RootLayout({ children }) {
         className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClerkProvider
+            appearance={{
+              theme: dark,
+            }}
           >
-        <ClerkProvider  appearance={{
-            theme: dark,
-        }}>  
-        <ConvexClientProvider>
-        
-        {/* Header */}
-        <Header />
+            <ConvexClientProvider>
+              {/* Header */}
+              <Header />
 
-        <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
-          {/* glow */} 
-          <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-            <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
-            <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
-          </div>
+              <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+                {/* glow */}
+                <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+                </div>
 
-          <div className="relative z-10 min-h-[70vh]">{children}</div>
+                <div className="relative z-10 min-h-[70vh]">{children}</div>
 
-          {/* Footer */}
-          <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-autp">
-            <div className="text-sm text-gray-400">Made by Anand Yadav</div>
-          </footer>
-          <Toaster richColors />
-        </main>
-        </ConvexClientProvider>
-        </ClerkProvider> 
-      </ThemeProvider>
+                {/* Footer */}
+                <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-autp">
+                  <div className="text-sm text-gray-400">
+                    Made by Anand Yadav
+                  </div>
+                </footer>
+                <Toaster richColors />
+              </main>
+            </ConvexClientProvider>
+          </ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
